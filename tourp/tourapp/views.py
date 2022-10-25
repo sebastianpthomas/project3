1905ae20234df57d1fdaae6a3d3424c5e11454ec
sebastian Thomas <sebptom@gmail.com>
2022-10-25 15:47:15 +0530

    added

---
+++ tourp/tourapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from . models import place
+from . models import team
+# Create your views here.
+def demo(request):
+    obj=place.objects.all()
+    obj1=team.objects.all()
+    return render(request,'index.html',{'result':obj,'result1':obj1})
